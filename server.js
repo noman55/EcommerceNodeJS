df64662c10c1437642f16e67c89ba2d0b88015c1
noman razvi <mdnomanrazvi07@gmail.com>
2019-07-20 13:57:42 +0530

    envi port added
    Signed-off-by: noman razvi <mdnomanrazvi07@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const SERVER_PORT = process.env.PORT || 3456
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,4 +9,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/api", require("./routes/api").route);
 
-app.listen(3456, () => console.log("server started on 3456"));
+app.listen(SERVER_PORT, () => console.log("server started on 3456"));
